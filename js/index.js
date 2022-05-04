233461475247d924b774d3584db641cb0cabd71b
Andrey Shalomitsky <akokha90@.mail.ru>
2022-05-04 11:40:49 +0500

    1

--- js/index.js
+++ js/index.js
@@ -16,3 +16,30 @@ correctVhWindow();
 document.querySelector(".burger-checkbox").addEventListener("click", function () {
   document.body.classList.toggle("body-hidden");
 });
+
+// ..........................................................swiper..............//
+var swiper1 = new Swiper(".swiper2", {
+  // Default parameters
+  slidesPerView: 0,
+  spaceBetween: 10,
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+
+  // Responsive breakpoints1
+  breakpoints: {
+    // when window width is >= 360px
+    320: {
+      slidesPerView: 1.17,
+      spaceBetween: 0,
+      initialSlide: 0,
+    },
+    // when window width is >= 768px
+    567.98: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+});
