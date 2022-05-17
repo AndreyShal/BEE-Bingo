01afc960917c0502345b412cdf800978715196af
Andrey Shalomitsky <akokha90@.mail.ru>
2022-05-17 11:51:34 +0500

    3

--- js/index.js
+++ js/index.js
@@ -102,24 +102,42 @@ function smoothScrollingPrinciples() {
 }
 smoothScrollingPrinciples();
 
-gsap.from(".vector-line", {
-  duration: 5,
-  opacity: 0,
-  stagger: 0.2,
-  ease: "elastic",
-  yoyo: true,
-  repeat: -1,
-  repeatDelay: 0,
-  delay: 0,
-});
+function correctVhWindow() {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-gsap.from(".vector-line", {
-  stagger: 0.2,
-  duration: 5,
-  rotate: 180,
-  transformOrigin: "center",
-  yoyo: true,
-  repeat: -1,
-  repeatDelay: 0,
-  delay: 0,
-});
+  // We listen to the resize event
+  window.addEventListener("resize", () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+}
+correctVhWindow();
+
+function imageTransformGsap() {
+  gsap.from(".vector-line", {
+    duration: 5,
+    opacity: 0,
+    stagger: 0.2,
+    ease: "elastic",
+    yoyo: true,
+    repeat: -1,
+    repeatDelay: 0,
+    delay: 0,
+  });
+
+  gsap.from(".vector-line", {
+    stagger: 0.2,
+    duration: 5,
+    rotate: 180,
+    transformOrigin: "center",
+    yoyo: true,
+    repeat: -1,
+    repeatDelay: 0,
+    delay: 0,
+  });
+}
+imageTransformGsap();
