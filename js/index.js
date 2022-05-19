6384477a6eec0675562e7bb4cec7e28e220ecb9c
Andrey Shalomitsky <akokha90@.mail.ru>
2022-05-19 17:51:58 +0500

    1

--- js/index.js
+++ js/index.js
@@ -91,7 +91,7 @@ function smoothScrollingPrinciples() {
       return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
     },
     getBoundingClientRect() {
-      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+      return { top: 0, left: 1000, width: window.innerWidth, height: window.innerHeight };
     },
 
     pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed",
